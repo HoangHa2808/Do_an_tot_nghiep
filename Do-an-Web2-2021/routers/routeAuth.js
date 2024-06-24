be4198f72ae936dd46799f41390b88d38a2c9f45
HoangHa2808 <thanha2808@gmail.com>
2024-06-24 12:23:25 +0700

    Do an tot nghiep

---
+++ Do-an-Web2-2021/routers/routeAuth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+
+const authController = require('../controllers/authController');
+
+router.get('/user-infomation', authController.getInfoUser);
+
+router.post('/new-account', authController.newAccount);
+router.post('/refresh-token', authController.refreshToken);
+router.post('/login-account', authController.loginAccount);
+router.post('/admin-login', authController.adminLoginAccount);
+router.post('/refresh-token-admin', authController.refreshTokenAdmin);
+
+module.exports = router;
